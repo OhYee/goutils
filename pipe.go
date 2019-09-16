28544624ccf10bec214c27a97a1e1324a113a2be
OhYee <oyohyee@oyohyee.com>
2019-09-16 10:54:28 +0800

    update: add Pipe struct

---
+++ pipe.go
@@ -0,0 +1,79 @@
+package goutil
+
+import (
+	"fmt"
+	"io"
+	"sync"
+)
+
+var errwrite = fmt.Errorf("Pipe has closed")
+
+// Pipe mutil thread supported write-read pipe buffer
+type Pipe struct {
+	data   chan byte
+	close  chan bool
+	closed bool
+	mutex  *sync.Mutex
+}
+
+// NewPipe create a pipe
+func NewPipe(length int, b ...[]byte) Pipe {
+	p := Pipe{
+		data:   make(chan byte, length),
+		close:  make(chan bool, 1),
+		closed: false,
+		mutex:  &sync.Mutex{},
+	}
+
+	data := make([]byte, 0)
+	for _, d := range b {
+		data = append(data, d...)
+	}
+	p.Write(data)
+	return p
+}
+
+// Read data from pipe
+func (p Pipe) Read(b []byte) (n int, err error) {
+	for i := 0; i < len(b); i++ {
+		select {
+		case d := <-p.data:
+			b[i] = d
+			n++
+		default:
+			if n == 0 && p.isClose() {
+				err = io.EOF
+			}
+			break
+		}
+	}
+	b = b[:n]
+	return
+}
+
+// Write data to pipe
+func (p Pipe) Write(b []byte) (n int, err error) {
+	if p.isClose() {
+		err = errwrite
+		return
+	}
+	for _, bb := range b {
+		p.data <- bb
+		n++
+	}
+	return
+}
+
+func (p Pipe) isClose() bool {
+	p.mutex.Lock()
+	closed := p.closed
+	p.mutex.Unlock()
+	return closed
+}
+
+// Close the pipe
+func (p *Pipe) Close() {
+	p.mutex.Lock()
+	p.closed = true
+	p.mutex.Unlock()
+}
---
+++ pipe_test.go
@@ -0,0 +1,61 @@
+package goutil
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestPipe_Read(t *testing.T) {
+	tests := []struct {
+		name string
+		r    io.Reader
+		p    Pipe
+	}{
+		{
+			name: "compare with bytes.buffer",
+			r:    bytes.NewBuffer([]byte{0x01, 0x02, 0x03, 0x04, 0x05}),
+			p:    NewPipe(16, []byte{0x01, 0x02, 0x03, 0x04, 0x05}),
+		},
+		{
+			name: "compare with bytes.buffer - part of data",
+			r:    bytes.NewBuffer([]byte{0x01}),
+			p:    NewPipe(16, []byte{0x01}),
+		},
+		{
+			name: "EOF",
+			r:    bytes.NewBuffer([]byte{}),
+			p:    NewPipe(16, []byte{}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data1 := make([]byte, 2, 4)
+			data2 := make([]byte, 2, 4)
+			data1[0] = 0x00
+			data2[0] = 0x00
+			tt.p.Close()
+			n1, err1 := tt.r.Read(data1)
+			n2, err2 := tt.p.Read(data2)
+			if !Equal(n1, n2) || !Equal(err1, err2) || !Equal(data1, data2) {
+				t.Errorf("%v %v %v != %v %v %v", n1, err1, data1, n2, err2, data2)
+			} else {
+				t.Logf("%v %v %v == %v %v %v", n1, err1, data1, n2, err2, data2)
+			}
+		})
+	}
+}
+
+func TestPipe_Write(t *testing.T) {
+	t.Run("write error", func(t *testing.T) {
+		p := NewPipe(16, []byte{0x01})
+		p.Close()
+		n, err := p.Write([]byte{0x02})
+		b := make([]byte, 16)
+		n2, err2 := p.Read(b)
+		b = b[:n2]
+		if n != 0 || !Equal(err, errwrite) || n2 != 1 || err2 != nil || !Equal(b, []byte{0x01}) {
+			t.Errorf("n=%d err=%v n2=%d err2=%v bytes=%v", n, err, n2, err2, b)
+		}
+	})
+}
